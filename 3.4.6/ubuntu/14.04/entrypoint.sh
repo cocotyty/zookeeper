e8b9243827f7f3168db8fb2811aec5e415cda69f
cocotyty <cocotyty@sina.com>
2016-01-07 17:34:13 +0800

    Update entrypoint.sh

--- 3.4.6/ubuntu/14.04/entrypoint.sh
+++ 3.4.6/ubuntu/14.04/entrypoint.sh
@@ -8,12 +8,12 @@ tickTime=2000
 initLimit=10
 syncLimit=5
 dataDir=/tmp/zookeeper
-clientPort=2181
+clientPort=$CLIENTPORT
 EOF
 
 # server.1=...
 if [ -n "$SERVERS" ]; then
-    python -c "print '\n'.join(['server.%i=%s:2888:3888' % (i + 1, x) for i, x in enumerate('$SERVERS'.split(','))])" >> /opt/zookeeper/conf/zoo.cfg
+    python -c "print '\n'.join(['server.%i=%s' % (i + 1, x) for i, x in enumerate('$SERVERS'.split(','))])" >> /opt/zookeeper/conf/zoo.cfg
 fi
 
 exec "$@"
